a375a165e9814eeff9e87d8c179e25fadf08f8af
PBelugin <pashaigood@gmail.com>
2014-10-24 10:04:23 +0400

    Изменена стркутра. Список треков из tmp.

--- src/app/scripts/controllers/player.js
+++ src/app/scripts/controllers/player.js
@@ -1,9 +1,9 @@
 'use strict';
+
+
 angular.module('app')
-    .controller('MainCtrl', function ($scope, $interval, Player) {
-        $scope.test = 123;
-        var TICK = 1000,
-            player = $scope.player = new Player();
+    .controller('PlayerCtrl', function ($scope, $interval, Player) {
+        var player = $scope.player = new Player();
 
         angular.extend(
             $scope,
@@ -44,45 +44,6 @@ angular.module('app')
             $scope.playlist = player.playlist;
 
         }
-
-        $scope.win = function() {
-            $scope.isWin = true;
-        }
-
-        /*
-        function startTrack() {
-            player.time = player.time || player.track.DurationSecs;
-
-
-            stop = $interval(tick,
-                TICK / 100,
-                Math.round(player.time / 1000)
-            );
-            stop.then(onStopTime);
-        }
-
-        function onStopTime() {
-            if(playlist[index]) {
-                startTrack();
-            }
-        }
-
-        function changeTrack() {
-            stopTrack();
-            player.track = playlist[index++];
-        }
-
-        function tick() {
-            player.percent = 100 - player.time / player.track.DurationSecs * 100;
-            player.time -= TICK;
-        }
-
-
-        function stopTrack() {
-            $interval.cancel(stop);
-            player.state = false;
-        }
-        */
 
         init();
     });
