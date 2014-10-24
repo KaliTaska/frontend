3fd02f54873bfbc98f4ef37fe22e018646ff86b1
PBelugin <pashaigood@gmail.com>
2014-10-24 15:50:37 +0400

    Исправлен баг с новым форматом данных и сториних плейлистов.

--- src/app/scripts/services/TaskList.js
+++ src/app/scripts/services/TaskList.js
@@ -8,7 +8,7 @@ angular.module('app')
             {"Tasks": [
                 {"TimeEntries": [
                     {"Id": 1, "Begin": "2014-10-23T23:56:52.4", "End": "2014-10-23T23:56:57.347", "TaskaId": 1, "IsActive": false}
-                ], "Id": 1, "Name": "Start Presentation", "Description": "Start presentation. Surprise everybody. Be awesome.", "EstimatedDurationSecs": 120, "DurationSecs": 5, "IsActive": true},
+                ], "Id": 1, "Name": "Start Presentation", "Description": "Start presentation. Surprise everybody. Be awesome.", "EstimatedDurationSecs": 10, "DurationSecs": 5, "IsActive": true},
                 {"TimeEntries": [], "Id": 2, "Name": "Introduction", "Description": "Intrdouctions. Short review of the concept of KaliTaska.", "EstimatedDurationSecs": 360, "DurationSecs": 0, "IsActive": true},
                 {"TimeEntries": [], "Id": 3, "Name": "Be cool!", "Description": "Just Do It!", "EstimatedDurationSecs": 9999, "DurationSecs": 0, "IsActive": true}
             ], "Id": 1, "Name": "Kalilaska Presentaion List", "Price": 0.00, "IsPremium": false},
