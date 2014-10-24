2a3f0d365f0c85706e3684d309c7dc497175deda
diiceas <forthepost@gmail.com>
2014-10-24 14:36:04 +0300

    - get rid of redundant navbar - minor refactoring - replace tasklist service with factory

--- src/app/scripts/services/Player.js
+++ src/app/scripts/services/Player.js
@@ -5,17 +5,17 @@ angular.module('app')
         var TICK = 1000;
         function Player() {
 
-            this.playlist = [
-                {"Tasks":[],"Id":2,"Name":"Rule the world","Descrition":"Wann rule u all, sux0rz!","Start":"\/Date(1412996660960)\/","DurationSecs":1500000,"HasChildren":false},
-                {"Tasks":[],"Id":2,"Name":"Rule other world","Descrition":"Wann rule u all!!, sux0rz!","Start":"\/Date(1412996660960)\/","DurationSecs":1500000 * 2,"HasChildren":false},
-                {"Tasks":[],"Id":2,"Name":"Rule the world","Descrition":"Wann rule u all, sux0rz!","Start":"\/Date(1412996660960)\/","DurationSecs":1500000,"HasChildren":false},
-                {"Tasks":[],"Id":2,"Name":"Rule the world","Descrition":"Wann rule u all, sux0rz!","Start":"\/Date(1412996660960)\/","DurationSecs":1500000,"HasChildren":false},
-                {"Tasks":[],"Id":2,"Name":"Rule the world","Descrition":"Wann rule u all, sux0rz!","Start":"\/Date(1412996660960)\/","DurationSecs":1500000,"HasChildren":false},
-                {"Tasks":[],"Id":2,"Name":"Rule the world","Descrition":"Wann rule u all, sux0rz!","Start":"\/Date(1412996660960)\/","DurationSecs":1500000,"HasChildren":false},
-                {"Tasks":[],"Id":2,"Name":"Rule the world","Descrition":"Wann rule u all, sux0rz!","Start":"\/Date(1412996660960)\/","DurationSecs":1500000,"HasChildren":false},
-                {"Tasks":[],"Id":2,"Name":"Rule the world","Descrition":"Wann rule u all, sux0rz!","Start":"\/Date(1412996660960)\/","DurationSecs":150000,"HasChildren":false},
-                {"Tasks":[],"Id":2,"Name":"Rule the world","Descrition":"Wann rule u all, sux0rz!","Start":"\/Date(1412996660960)\/","DurationSecs":150000,"HasChildren":false},
-            ];
+            // this.playlist = [
+            //     {"Tasks":[],"Id":2,"Name":"Rule the world","Descrition":"Wann rule u all, sux0rz!","Start":"\/Date(1412996660960)\/","DurationSecs":1500000,"HasChildren":false},
+            //     {"Tasks":[],"Id":2,"Name":"Rule other world","Descrition":"Wann rule u all!!, sux0rz!","Start":"\/Date(1412996660960)\/","DurationSecs":1500000 * 2,"HasChildren":false},
+            //     {"Tasks":[],"Id":2,"Name":"Rule the world","Descrition":"Wann rule u all, sux0rz!","Start":"\/Date(1412996660960)\/","DurationSecs":1500000,"HasChildren":false},
+            //     {"Tasks":[],"Id":2,"Name":"Rule the world","Descrition":"Wann rule u all, sux0rz!","Start":"\/Date(1412996660960)\/","DurationSecs":1500000,"HasChildren":false},
+            //     {"Tasks":[],"Id":2,"Name":"Rule the world","Descrition":"Wann rule u all, sux0rz!","Start":"\/Date(1412996660960)\/","DurationSecs":1500000,"HasChildren":false},
+            //     {"Tasks":[],"Id":2,"Name":"Rule the world","Descrition":"Wann rule u all, sux0rz!","Start":"\/Date(1412996660960)\/","DurationSecs":1500000,"HasChildren":false},
+            //     {"Tasks":[],"Id":2,"Name":"Rule the world","Descrition":"Wann rule u all, sux0rz!","Start":"\/Date(1412996660960)\/","DurationSecs":1500000,"HasChildren":false},
+            //     {"Tasks":[],"Id":2,"Name":"Rule the world","Descrition":"Wann rule u all, sux0rz!","Start":"\/Date(1412996660960)\/","DurationSecs":150000,"HasChildren":false},
+            //     {"Tasks":[],"Id":2,"Name":"Rule the world","Descrition":"Wann rule u all, sux0rz!","Start":"\/Date(1412996660960)\/","DurationSecs":150000,"HasChildren":false},
+            // ];
 
             this.playlist = [
                 {"Tasks":[],"Id":1,"Name":"Be cool!","Description":"Rule it!","Start":"2014-10-11T14:58:06.427","DurationSecs":160000,"IsInPlay":false,"IsActive":false,"ShowNext":false,"ShowNotification":false,"HasChildren":false},{"Tasks":[{"Tasks":[],"Id":3,"Name":"Introduction","Description":"Intrdouctions. Short review of the concept.","Start":"2014-10-11T14:58:06.427","DurationSecs":60000,"IsInPlay":false,"IsActive":false,"ShowNext":false,"ShowNotification":false,"HasChildren":false},{"Tasks":[],"Id":4,"Name":"Say something awesome","Description":"Now say something cool to own them!","Start":"2014-10-11T15:00:06.427","DurationSecs":460000,"IsInPlay":false,"IsActive":false,"ShowNext":false,"ShowNotification":true,"HasChildren":false},{"Tasks":[],"Id":5,"Name":"Be cool!","Description":"Just Do It!","Start":"2014-10-11T15:03:26.427","DurationSecs":60000,"IsInPlay":false,"IsActive":false,"ShowNext":false,"ShowNotification":false,"HasChildren":false}],"Id":2,"Name":"Start Presentation","Description":"Start Presentation on Kalitaska software","Start":"2014-10-11T14:58:06.427","DurationSecs":60000,"IsInPlay":false,"IsActive":false,"ShowNext":false,"ShowNotification":false,"HasChildren":true},{"Tasks":[],"Id":3,"Name":"Introduction","Description":"Intrdouctions. Short review of the concept.","Start":"2014-10-11T14:58:06.427","DurationSecs":60000,"IsInPlay":false,"IsActive":false,"ShowNext":false,"ShowNotification":false,"HasChildren":false},{"Tasks":[],"Id":4,"Name":"Say something awesome","Description":"Now say something cool to own them!","Start":"2014-10-11T15:00:06.427","DurationSecs":60000,"IsInPlay":false,"IsActive":false,"ShowNext":false,"ShowNotification":true,"HasChildren":false},
--- src/app/scripts/services/TaskList.js
+++ src/app/scripts/services/TaskList.js
@@ -1,23 +1,27 @@
-/**
- * Author: Sergei Kasoverskij
- * E-Mail: joycollector@gmail.com
- */
-angular.module('app')
-    .service('taskLists', function () {
-        this.taskLists = [
-            {"Tasks": [
-                {"TimeEntries": [
-                    {"Id": 1, "Begin": "2014-10-23T23:56:52.4", "End": "2014-10-23T23:56:57.347", "TaskaId": 1, "IsActive": false}
-                ], "Id": 1, "Name": "Start Presentation", "Description": "Start presentation. Surprise everybody. Be awesome.", "EstimatedDurationSecs": 120, "DurationSecs": 5, "IsActive": true},
-                {"TimeEntries": [], "Id": 2, "Name": "Introduction", "Description": "Intrdouctions. Short review of the concept of KaliTaska.", "EstimatedDurationSecs": 360, "DurationSecs": 0, "IsActive": true},
-                {"TimeEntries": [], "Id": 3, "Name": "Be cool!", "Description": "Just Do It!", "EstimatedDurationSecs": 9999, "DurationSecs": 0, "IsActive": true}
-            ], "Id": 1, "Name": "Kalilaska Presentaion List", "Price": 0.00, "IsPremium": false},
-            {"Tasks": [
-                {"TimeEntries": [], "Id": 4, "Name": "Introduction", "Description": "Well, we have quite a simple interface, so just click here and there... You'll get used to it, I asure you.", "EstimatedDurationSecs": 120, "DurationSecs": 0, "IsActive": true}
-            ], "Id": 2, "Name": "KaliTaska Tutorial", "Price": 0.00, "IsPremium": false}
-        ];
-        this.current = 0;
-    });
+(function(){
 
+    var taskLists = function(){
+      var taskLists = [
+      {"Tasks": [
+      {"TimeEntries": [
+      {"Id": 1, "Begin": "2014-10-23T23:56:52.4", "End": "2014-10-23T23:56:57.347", "TaskaId": 1, "IsActive": false}
+      ], "Id": 1, "Name": "Start Presentation", "Description": "Start presentation. Surprise everybody. Be awesome.", "EstimatedDurationSecs": 120, "DurationSecs": 5, "IsActive": true},
+      {"TimeEntries": [], "Id": 2, "Name": "Introduction", "Description": "Intrdouctions. Short review of the concept of KaliTaska.", "EstimatedDurationSecs": 360, "DurationSecs": 0, "IsActive": true},
+      {"TimeEntries": [], "Id": 3, "Name": "Be cool!", "Description": "Just Do It!", "EstimatedDurationSecs": 9999, "DurationSecs": 0, "IsActive": true}
+      ], "Id": 1, "Name": "Kalilaska Presentaion List", "Price": 0.00, "IsPremium": false},
+      {"Tasks": [
+      {"TimeEntries": [], "Id": 4, "Name": "Introduction", "Description": "Well, we have quite a simple interface, so just click here and there... You'll get used to it, I asure you.", "EstimatedDurationSecs": 120, "DurationSecs": 0, "IsActive": true}
+      ], "Id": 2, "Name": "KaliTaska Tutorial", "Price": 0.00, "IsPremium": false}
+      ];
+      var current = 0;
 
+      return {
+        taskLists: taskLists,
+        current: current
+    }
+};
 
+var app = angular.module("app");
+app.factory("taskLists", taskLists);
+
+}());
